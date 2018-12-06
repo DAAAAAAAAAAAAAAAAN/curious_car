0fc99615656e4ca29d46f4ed4645b4640e504d70
aronhammond <hammond756@live.nl>
2018-12-06 09:35:49 +0100

    added class for replay memory

---
+++ ReplayMemory.py
@@ -0,0 +1,21 @@
+import random
+
+__author__ = 'Aron'
+
+class ReplayMemory:
+
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+
+    def push(self, transition):
+        self.memory += [transition]
+
+        if len(self.memory) > self.capacity:
+            self.memory = self.memory[1:]
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
